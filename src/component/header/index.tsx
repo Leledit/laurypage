cd2c048ed6cc5e850050a9ff0ee37f8dfff82e25
Leandro ricardo caixeta junior <profile.caixeta99@gmail.com>
2023-05-09 20:51:58 -0300

    Terminado a codificação da versão 'mobile' do componente 'header'

---
+++ src/component/header/index.tsx
@@ -0,0 +1,18 @@
+import styles from './index.module.scss';
+export function Header(){
+    return(
+        <header className={styles.header}>
+            <div className={styles.headerContainer}>
+                <div className={styles.headerInfos}>
+                    <h1 className={styles.headerTitulo}>Tenha o seu corpo dos sonhos</h1>
+                    <h2 className={styles.headerSubTitle}>cansado de olhar para o espelho e não ver a sua melhor versão, venha se consultar comigo e <strong>descubra como ter resultados reais na sua dieta</strong></h2>
+                </div>
+                <form className={styles.headerForm}>
+                    <input type='text' placeholder='Seu nome:' className={styles.headerFormInput} />
+                    <input type='email' placeholder='Seu melhor email:' className={styles.headerFormInput}/>
+                    <input type='submit' value="Quero me consultar" className={styles.headerFormInputSubmit}></input>
+                </form>
+            </div>
+        </header>
+    )
+}
